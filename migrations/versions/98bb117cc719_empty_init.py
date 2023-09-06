c3e91bd404811c7b8e203604a566abe5283d4510
victor odambatafwa <omondivictoras@gmail.com>
2023-09-06 23:02:31 +0300

    created librarys db file

---
+++ migrations/versions/98bb117cc719_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 98bb117cc719
+Revises: 
+Create Date: 2023-09-06 22:53:25.448090
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '98bb117cc719'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
